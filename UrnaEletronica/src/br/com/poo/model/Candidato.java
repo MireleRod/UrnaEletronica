5a2987435d596b82440065597d803f84bcf86924
glendaasilva <glendasilva0201@gmail.com>
2025-06-27 14:46:44 -0300

    Candidato.java

--- UrnaEletronica/src/br/com/poo/model/Candidato.java
+++ UrnaEletronica/src/br/com/poo/model/Candidato.java
@@ -9,5 +9,34 @@ package br.com.poo.model;
  * @author 232.975909
  */
 public class Candidato {
-    
+    private int numero;
+    private String nome;
+    private String partido;
+    private String caminhoImagem;
+
+    public Candidato(int numero, String nome, String partido, String caminhoImagem) {
+        this.numero = numero;
+        this.nome = nome;
+        this.partido = partido;
+        this.caminhoImagem = caminhoImagem;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public String getCaminhoImagem() {
+        return caminhoImagem;
+    }
 }
+
+    
+
