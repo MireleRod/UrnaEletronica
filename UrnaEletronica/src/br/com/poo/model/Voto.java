9ca1a84fd25da546519bb73d76ce6d17d7d20862
glendaasilva <glendasilva0201@gmail.com>
2025-06-27 14:39:34 -0300

    Criei a camada model
    Criei a classe Candidato, a classe Voto e a enum TipoVoto

---
+++ UrnaEletronica/src/br/com/poo/model/Voto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.poo.model;
+
+/**
+ *
+ * @author 232.975909
+ */
+public class Voto {
+    
+}
