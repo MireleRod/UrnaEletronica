13abb789601cf7daef8a764acbd631136de1cd8f
glendaasilva <glendasilva0201@gmail.com>
2025-06-30 15:41:34 -0300

    Update Mongodb.java

--- UrnaEletronimaMaven/src/main/java/br/com/poo/Persistence/Mongodb.java
+++ UrnaEletronimaMaven/src/main/java/br/com/poo/Persistence/Mongodb.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,6 +9,36 @@ package br.com.poo.Persistence;
  *
  * @author 232.975909
  */
-public class Mongodb {
+
     
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import br.com.poo.model.Voto;
+import br.com.poo.model.TipoVoto;
+
+public class BancoDeDadosMongo {
+
+    private MongoDatabase database;
+
+    public BancoDeDadosMongo() {
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        database = mongoClient.getDatabase("urna_eletronica");
+    }
+
+    public void salvarVoto(Voto voto) {
+        MongoCollection<Document> votosCollection = database.getCollection("votos");
+
+        Document doc = new Document("numeroDigitado", voto.getNumeroDigitado())
+                .append("tipo", voto.getTipo().toString());
+
+        votosCollection.insertOne(doc);
+    }
 }
+
+
+
