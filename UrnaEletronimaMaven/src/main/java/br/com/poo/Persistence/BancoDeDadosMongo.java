96fcd967b3f4295b3d3bfbb631eab88f09d84918
glendaasilva <glendasilva0201@gmail.com>
2025-06-30 16:08:01 -0300

    Update BancoDeDadosMongo.java
    Atualizacao 2.0 da classe BancoDeDadosMongo

--- UrnaEletronimaMaven/src/main/java/br/com/poo/Persistence/BancoDeDadosMongo.java
+++ UrnaEletronimaMaven/src/main/java/br/com/poo/Persistence/BancoDeDadosMongo.java
@@ -9,34 +9,97 @@
  */
 package br.com.poo.persistence;
 
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
+import br.com.poo.model.*;
+import com.mongodb.client.*;
 import org.bson.Document;
-
-import br.com.poo.model.Voto;
-import br.com.poo.model.TipoVoto;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BancoDeDadosMongo {
 
-    private MongoDatabase database;
+    private final MongoClient mongoClient;
+    private final MongoDatabase database;
+    private final MongoCollection<Document> votosCollection;
+    private final MongoCollection<Document> candidatosCollection;
+    private final MongoCollection<Document> partidosCollection;
 
     public BancoDeDadosMongo() {
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-        database = mongoClient.getDatabase("urna_eletronica");
+        mongoClient = MongoClients.create("mongodb://localhost:27017");
+        database = mongoClient.getDatabase("urna");
+        votosCollection = database.getCollection("votos");
+        candidatosCollection = database.getCollection("candidatos");
+        partidosCollection = database.getCollection("partidos");
     }
 
-    public void salvarVoto(Voto voto) {
-        MongoCollection<Document> votosCollection = database.getCollection("votos");
-
+    public void registrarVoto(Voto voto) {
         Document doc = new Document("numeroDigitado", voto.getNumeroDigitado())
-                .append("tipo", voto.getTipo().toString());
-
+                .append("tipo", voto.getTipo().name());
         votosCollection.insertOne(doc);
     }
+
+    public Candidato buscarCandidatoPorNumero(String numero) {
+        Document filtro = new Document("numero", numero);
+        Document doc = candidatosCollection.find(filtro).first();
+
+        if (doc != null) {
+            String nome = doc.getString("nome");
+            String siglaPartido = doc.getString("siglaPartido");
+            String caminhoImagem = doc.getString("caminhoImagem");
+
+            Partido partido = buscarPartidoPorSigla(siglaPartido);
+            return new Candidato(numero, nome, partido, caminhoImagem);
+        }
+        return null;
+    }
+
+    public Partido buscarPartidoPorSigla(String sigla) {
+        Document filtro = new Document("sigla", sigla);
+        Document doc = partidosCollection.find(filtro).first();
+
+        if (doc != null) {
+            String nome = doc.getString("nome");
+            return new Partido(nome, sigla);
+        }
+        return null;
+    }
+
+    public List<Voto> getTodosVotos() {
+        List<Voto> votos = new ArrayList<>();
+        for (Document doc : votosCollection.find()) {
+            String numeroDigitado = doc.getString("numeroDigitado");
+            TipoVoto tipo = TipoVoto.valueOf(doc.getString("tipo"));
+            votos.add(new Voto(numeroDigitado, tipo));
+        }
+        return votos;
+    }
+
+    public List<Candidato> getTodosCandidatos() {
+        List<Candidato> candidatos = new ArrayList<>();
+        for (Document doc : candidatosCollection.find()) {
+            String numero = doc.getString("numero");
+            String nome = doc.getString("nome");
+            String siglaPartido = doc.getString("siglaPartido");
+            String caminhoImagem = doc.getString("caminhoImagem");
+
+            Partido partido = buscarPartidoPorSigla(siglaPartido);
+            candidatos.add(new Candidato(numero, nome, partido, caminhoImagem));
+        }
+        return candidatos;
+    }
+
+    public List<Partido> getTodosPartidos() {
+        List<Partido> partidos = new ArrayList<>();
+        for (Document doc : partidosCollection.find()) {
+            String nome = doc.getString("nome");
+            String sigla = doc.getString("sigla");
+            partidos.add(new Partido(nome, sigla));
+        }
+        return partidos;
+    }
+
+    public void limparDados() {
+        votosCollection.deleteMany(new Document());
+        candidatosCollection.deleteMany(new Document());
+        partidosCollection.deleteMany(new Document());
+    }
 }
-
-
-    
-
