20f6214614801aab2f7d8d4fb5b9abd05b14bafe
hebertzinx <ipecaeta6@gmail.com>
2025-06-30 14:02:15 -0300

    Update Partido.java

--- UrnaEletronica/src/br/com/poo/model/Partido.java
+++ UrnaEletronica/src/br/com/poo/model/Partido.java
@@ -8,6 +8,39 @@ package br.com.poo.model;
  *
  * @author 232.976393
  */
+import java.util.ArrayList;
+  import java.util.List;
+
 public class Partido {
+  
+    private String nome;
+    private String sigla;
+    private List<Candidato> candidatos;
+
+    public Partido(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.candidatos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public List<Candidato> getCandidatos() {
+        return candidatos;
+    }
+
+    public void adicionarCandidato(Candidato candidato) {
+        candidatos.add(candidato);
+    }
+}
+
+
+    
     
 }
